646f6e3368bb634896eec17eab1be08e893639c3
Gavintime <16593223+Gavintime@users.noreply.github.com>
2022-12-14 14:01:01 -0800

    Finish Day5 part 1

--- Day5/Part1.py
+++ Day5/Part1.py
@@ -3,8 +3,12 @@
 import sys
 from collections import deque
 
+
+# TODO: allow for multi digit/non numerical stack names
+
+
 # returns a dict of deques parsed from input, as well as index of first instruction
-def parse_crates(crates: list[list[str]]) -> list[dict[deque[str]] | int]:
+def parse_crates(crates: list[str]) -> tuple[dict[str,deque[str]],int]:
 
     if len(crates[0]) < 3:
         print("Input does not start with a valid crate drawing. Exiting...")
@@ -40,22 +44,84 @@ def parse_crates(crates: list[list[str]]) -> list[dict[deque[str]] | int]:
             stack_map[crates[stack_label_index][i]].append(crates[current_level][i])
         current_level -= 1
 
-    return [stack_map, stack_label_index + 2]
+    return (stack_map, stack_label_index + 2)
 
 
-def process_instructions(stack_map: dict[deque[str]], instructions: list[str]) -> str:
+# returns the first number of the given string, terminated by a space
+# also returns the index directly after the terminating space
+def get_number(input: str) -> tuple[int, int]:
 
-    for inst in instructions:
+    # get crate count of instruction
+    number = ''
+    index = 0
+    while input[index] != ' ':
+        if not input[index].isnumeric():
+            print(f"Instruction starting at \"{input}\" does not follow correct format. Exiting...")
+            sys.exit(1)
+        number += input[index]
+        index += 1
 
-        if inst[0:5] != 'move ':
-            print(f"Instruction {inst} does not follow correct format. Exiting...")
+    return (int(number), index + 1)
+
+
+def parse_instructions(stack_map: dict[str,deque[str]], inst_raw: list[str]) -> list[tuple[int,str,str]]:
+
+    inst_list: list[tuple[int,str,str]] = []
+
+
+    for inst in inst_raw:
+
+        if inst == '\n':
+            break
+
+        inst = inst[:-1]
+
+        crate_count: int
+        source_stack: str
+        dest_stack: str
+
+        if len(inst) < 18 or inst[0:5] != 'move ':
+            print(f"Instruction \"{inst}\" does not follow correct format. Exiting..1.")
             sys.exit(1)
 
-        # get crate count of instruction
-        crate_count: int = ''
+        # get crate count of instruction, index 5 must be the index of the left
+        # most digit of the the crate size
         inst_index = 5
+        crate_count, temp_index = get_number(inst[inst_index:])
+        inst_index += temp_index
 
-        while inst[inst_index] != ' '
+
+        if inst[inst_index:inst_index+5] != 'from ':
+            print(f"Instruction \"{inst}\" does not follow correct format. Exiting...2")
+            sys.exit(1)
+
+        # get name (digit) of the source/destination crate stacks
+        inst_index += 5
+        source_stack = inst[inst_index]
+        if inst[inst_index+1:inst_index+5] != ' to ':
+            print(f"Instruction \"{inst}\" does not follow correct format. Exiting...3")
+            sys.exit(1)
+        dest_stack = inst[inst_index+5]
+
+        inst_list.append((crate_count, source_stack, dest_stack))
+
+    return inst_list
+
+
+def process_instruction(stack_map: dict[str,deque[str]], inst_list: list[tuple[int,str,str]]) -> str:
+
+    answer = ''
+
+    for inst in inst_list:
+        # pop/push as many times as the instruction says
+        for _ in range(inst[0]):
+            stack_map[inst[2]].append(stack_map[inst[1]].pop())
+
+    # add the top value of each stack to the answer and return
+    # TODO: handle empty stacks?
+    for i in range(1, len(stack_map) + 1):
+        answer += stack_map[str(i)][-1]
+    return answer
 
 
 def main():
@@ -78,8 +144,9 @@ def main():
         sys.exit(1)
 
     stack_map, inst_index = parse_crates(lines)
-    output = process_instructions(stack_map, lines[inst_index:])
-    print(output)
+    inst_list = parse_instructions(stack_map, lines[inst_index:])
+    answer = process_instruction(stack_map, inst_list)
+    print(answer)
 
 
 if __name__ == '__main__':
